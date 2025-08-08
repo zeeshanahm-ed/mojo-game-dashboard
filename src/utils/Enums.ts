02eff07a600337a2cac9a996e9da790ffcd39e10
zeeshanahm-ed <zeeshan.dev@irsolutions.tech>
2025-08-08 17:24:08 +0500

    add enum and interface

---
+++ src/utils/Enums.ts
@@ -0,0 +1,7 @@
+export enum ROLES {
+    SUPER_ADMIN = 'super_admin',
+    CONTENT_MANAGER = 'content_manager',
+    FINANCE_MANAGER = 'finance_manager',
+    READ_ONLY = 'read_only',
+    USER = 'user',
+}
--- src/utils/Interfaces.tsx
+++ src/utils/Interfaces.tsx
@@ -1,6 +1,23 @@
-interface CategoryInterface {
+export interface CategoryInterface {
     id: number;
     categoryName: string;
     categoryType: string;
     section: string;
+};
+
+export interface PromoCodeRecord {
+    id: string;
+    createdDate: string;
+    promoCode: string;
+    percentage: number;
+    usageLimit: number;
+    assigned: "All users" | "custom users";
+    promoAnalyticsLink: string;
+    expiryDate: string;
+    assignTo: "all" | "custom";
+    customUsers?: string[];
+    discountDuration: {
+        startDate: string;
+        endDate: string;
+    };
 }
