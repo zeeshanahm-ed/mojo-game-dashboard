d5f7032f35915756edbed6445bd39e24a3e7f230
zeeshanahm-ed <zeeshan.dev@irsolutions.tech>
2025-08-08 17:23:43 +0500

    remove unused code

--- src/utils/caseutils.ts
+++
@@ -1,6 +0,0 @@
-export const caseStatus = [
-    {id: 0, status:'completed',title:'Completed'},
-    {id:2, status:'inprogress',title:'In Progress'},
-    {id:1, status:'pending',title:'Pending'},
-
-]
