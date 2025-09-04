87c09254e1c10b6d71adcfc1bd4a59afe93d17a4
zeeshanahm-ed <zeeshan.dev@irsolutions.tech>
2025-09-04 16:44:20 +0500

    Update environment configurations and deployment script
    - Changed VITE_API_BASE_URL in .env.example, .env.dev, and .env.prod to point to the new server address.
    - Updated the deployment script to sync the build output to the new S3 bucket for the admin dashboard.

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,6 @@ fi
 cp envs/.env.prod .env
 npm install
 npm run build
-aws s3 sync  --profile mojo-app ./dist s3://mojo-frontend-app
+aws s3 sync  --profile mojo-app ./dist s3://mojo-admin-dashboard
 aws cloudfront create-invalidation --profile mojo-app --distribution-id E36EJJPEZKZYME --paths "/*"
 # cp envs/.env.local .env
